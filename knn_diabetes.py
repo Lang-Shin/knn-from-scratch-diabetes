cb20cd6d49ab3b0324ffeb4ccf3460911db1bea8
Lang-Shin <bajuracharles@gmail.com>
2026-04-25 02:20:18 +0800

    Feat : Fixed missing data (Glucose, Blood Pressure, Skin Thickness, Insulin, BMI)

--- knn_diabetes.py
+++ knn_diabetes.py
@@ -9,6 +9,19 @@ def count_missing_val(df, col):
 
 df = pd.read_csv("dataset/data.csv")
 
+"""
+    Handle missing data to columns such as Glucose, Blood Pressure, Skin Thickness, Insulin, BMI.
+    Using median to fill missing data
+"""
+
+fix_cols = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']      # Cols need to fix (0 -> median)
+
+df[fix_cols] = df[fix_cols].replace(0, np.nan)                                  # replace 0 with NaN
+
+for col in fix_cols:
+    median = df[col].median()   
+    df[col] = df[col].fillna(median)                                            # Replace NaN with median(col)
+
 print(df.head(10), "\n\n")
 print(count_missing_val(df, "Glucose"))
 print(count_missing_val(df, "BloodPressure"))
